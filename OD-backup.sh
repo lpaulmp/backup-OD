85dbd0461a2e2849c8d4b95507d1a2e62ee00604
Paul Montero <pmontero@stackbuilder.com>
2015-05-07 14:49:40 -0500

    change path

--- OD-backup.sh
+++ OD-backup.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 # MAKE OPEN DIRECTORY DB BACKUP EVERY DAY
-path="/Users/pmontero/Documents"
-dateDB=`date "+%Y_%m_%d_%H_%M"`
-dateDir=`date "+%Y_%m_%H"`
+path="/path/to/save/backup"
+dateDB=`date "+%Y_%m_%d_%H%M"`
+dateDir=`date "+%Y_%m"`
 
 # IF NOT EXIST CREATE A FOLDER PER MONTH 
 [ ! -d  $path/OpenDiretoryDB_$dateDir ] && `mkdir -p $path/OpenDiretoryDB_$dateDir`
 # BACKUP DB OPEN DIRECTORY
-#sudo slapconfig -backupdb $path/OpenDirtoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
-touch $path/OpenDiretoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
+# TEST OF PATH touch $path/OpenDiretoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
+sudo slapconfig -backupdb $path/OpenDirtoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
 echo "db_backup_$dateDB" >> $path/db_backup.log 2>&1
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Cron Jobs adjust the period of execution of the script.
 The follow steps help to create the a job and ajust to your preferences
 
 * `$ EDITOR=vim/nano/emacs crontab -e`
-* ` 00 06 * * *  /your/path/script/`  Will run the job 1 time per day at 1 pm at 6 am
+* ` 00 06 * * *  /your/path/script/`  Will run the job 1 time per day at 6 am
 
 Also will need a control of population of files with a cronjob which will remove files olders for more than year.
 
