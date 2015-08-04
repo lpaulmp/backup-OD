0b4c5172e7fc523c8f5ab44505790e7a7ac6006f
Paul Montero <pmontero@stackbuilders.com>
2015-08-04 15:45:46 -0500

    Use expect to pass a password to slapconfig

--- OD-backup.sh
+++ OD-backup.sh
@@ -1,13 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
 # MAKE OPEN DIRECTORY DB BACKUP EVERY DAY
-path="/path/to/save/ODbackup"
+path="/path/to/save"
 dateDB=`date "+%Y_%m_%d_%H%M"`
 dateDir=`date "+%Y_%m"`
+# ASSIGN PASSWORD TO THE BACKUP
+db_pass="password"
 
-# IF NOT EXIST CREATE A FOLDER PER MONTH 
-[ ! -d  $path/OpenDiretoryDB_$dateDir ] && `mkdir -p $path/OpenDiretoryDB_$dateDir`
-# BACKUP OPEN DIRECTORY DB
-# TEST OF PATH touch $path/OpenDiretoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
-sudo slapconfig -backupdb $path/OpenDirtoryDB_$dateDir/db_backup_$dateDB >> $path/db_backup.log 2>&1 && \
-echo "db_backup_$dateDB" >> $path/db_backup.log 2>&1
+# IF NOT EXIST CREATE A FOLDER PER MONTH
+[ ! -d  $path/OpenDiretoryDB_$dateDir ] && `mkdir -p $path/OpenDirectoryDB_$dateDir`
+
+expect -c "
+        spawn slapconfig -backupdb \"$path/OpenDirectoryDB_$dateDir/db_backup_$dateDB\"
+        expect \"Enter archive password:\"
+        send \"$db_pass\r\"
+        interact
+" >> $path/db_backup.log 2>&1
+
+echo "`date` Backup od_backup_$dateDB created successfully" >> $path/db_backup.log 2>&1
