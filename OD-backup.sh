aba20304a00878e269a9c7e97262dca51eba5d93
Paul Montero <pmontero@stackbuilders.com>
2015-08-05 12:16:28 -0500

    Add timeout into expect to wait fish dump and write into log when fail

--- OD-backup.sh
+++ OD-backup.sh
@@ -10,11 +10,21 @@ db_pass="password"
 # IF NOT EXIST CREATE A FOLDER PER MONTH
 [ ! -d  $path/OpenDiretoryDB_$dateDir ] && `mkdir -p $path/OpenDirectoryDB_$dateDir`
 
+# SET TIME OUT TO ALLOW FINISH THE DUMP 
 expect -c "
-        spawn slapconfig -backupdb \"$path/OpenDirectoryDB_$dateDir/db_backup_$dateDB\"
-        expect \"Enter archive password:\"
-        send \"$db_pass\r\"
-        interact
+  set timeout 150
+  spawn /usr/sbin/slapconfig -backupdb \"$path/OpenDirectoryDB_$dateDir/db_backup_$dateDB\"
+  expect \"Enter archive password:\"
+  send \"$db_pass\r\"
+  expect eof
+  exit
 " >> $path/db_backup.log 2>&1
 
-echo "`date` Backup od_backup_$dateDB created successfully" >> $path/db_backup.log 2>&1
+if [ $? -eq 0 ] && [ -e $path/OpenDirectoryDB_$dateDir/db_backup_$dateDB.sparseimage ]
+then
+  echo "`date` Backup od_backup_$dateDB created successfully" >> $path/db_backup.log 2>&1
+  exit 0
+else
+  echo "`date` Backup od_backup_$dateDB fail" >> $path/db_backup.log 2>&1
+  exit 1
+fi
